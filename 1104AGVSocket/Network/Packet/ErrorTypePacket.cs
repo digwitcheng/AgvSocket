dc692db1197db47da6623b8cdeb4407d30df8fac
digwit <1046541763@qq.com>
2017-12-13 23:04:17 +0800

    ErrorTypePacket

---
+++ 1104AGVSocket/Network/Packet/ErrorTypePacket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGVSocket.Network.Packet
+{
+    class ErrorTypePacket:ReceiveBasePacket
+    {
+        public override void Receive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override byte NeedLen()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
